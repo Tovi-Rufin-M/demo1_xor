c06145ccae92b4e817dbe34104a4114e81448c46
2V_Rufin M. <rufintovi11@gmail.com>
2026-04-14 23:45:03 +0800

    add

---
+++ Demo1_Xor/testbench.v
@@ -0,0 +1,28 @@
+// testbench for xor module
+
+module testbench;
+	// tb signals
+	reg  a;
+	reg  b;
+	wire y;
+	
+	
+	//insantiate xor module
+	xor_gate dut(
+		.Y(y),
+		.A(a),
+		.B(b)
+	);
+	
+	
+	// apply stimuli
+	
+	initial begin
+
+	a =0; b =0; #10;
+	a =0; b =1; #10;
+	a =1; b =0; #10;
+	a =1; b =1; #10;
+	end 
+	
+endmodule
---
+++ Demo1_Xor/xor_gate.v
@@ -0,0 +1,28 @@
+/*
+xor gate
+
+description:
+    this is an xor gate using gate lavel modeling
+
+design tech:
+    macalipsay, tovi R.
+
+date:
+    10 april 2026
+*/
+module xor_gate(Y,A,B);
+    /*ports*/
+    input A;
+    input B;
+    output Y;
+
+    /*nets*/
+    wire w1,w2,w3,w4;
+
+    /*logic*/
+    not u1(w1, A);
+    and u2(w3, w1, B);
+    or u5(Y, w3,w4);
+    and u4(w4, A, w2);
+    not u3(w2, B);
+endmodule
